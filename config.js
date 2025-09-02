26084d709962c09a241dcc833bb7714b28ed3842
Ankan5960 <ankanmaity27@gmail.com>
2025-09-03 00:38:37 +0530

    some changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
     credentials:{
-        client_id:"VAo79I9Uonpxhpa4GniqA31b1vt0usdxGXonraLNGUVtQc20",
-        client_secret:"BbkBDACxblP2jyNCrVfaLAouDkwip4eApGGUcwCiZlt19iHuouYTEiZyy4Ns6VhR"
+        client_id:"***",
+        client_secret:"**"
     },
     scopes:{
         internal: ['bucket:create','bucket:read','bucket:delete','data:read','data:create','data:write'],
