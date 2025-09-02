68c626242f1b3defd92db701c785ffa8b1a64020
Ankan Maity <104480693+Ankan5960@users.noreply.github.com>
2025-09-03 00:35:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+    credentials:{
+        client_id:"VAo79I9Uonpxhpa4GniqA31b1vt0usdxGXonraLNGUVtQc20",
+        client_secret:"BbkBDACxblP2jyNCrVfaLAouDkwip4eApGGUcwCiZlt19iHuouYTEiZyy4Ns6VhR"
+    },
+    scopes:{
+        internal: ['bucket:create','bucket:read','bucket:delete','data:read','data:create','data:write'],
+        public:['viewables:read']
+    }
+}
